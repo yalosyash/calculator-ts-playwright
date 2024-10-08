d6ce65fe70506ba0c0ba10ad1ce2298b52435e74
Vladislav Klepikov <ya.losyash@yandex.ru>
2024-10-08 18:05:27 +0300

    added opening of report

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,25 +6,28 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: [["list"], ["html"]],
+  reporter: [
+    ["list"],
+    ["html"],
+  ],
   use: {
     trace: "on-first-retry",
   },
 
   projects: [
-    // {
-    //   name: "chromium",
-    //   use: { ...devices["Desktop Chrome"] },
-    // },
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
 
     {
       name: "firefox",
       use: { ...devices["Desktop Firefox"] },
     },
 
-    // {
-    //   name: "webkit",
-    //   use: { ...devices["Desktop Safari"] },
-    // },
+    {
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
+    },
   ],
 });
